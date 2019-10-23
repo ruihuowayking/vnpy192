e7f26bafc0adacc79f65141d7b4f3cd4898e84c7
trade <leonzh18@hotmail.com>
2019-10-23 21:21:09 +0800

    sql

---
+++ examples/CtaAna/sql_log.sql
@@ -0,0 +1,84 @@
+INSERT INTO tradeorders (
+                            remark2,
+                            remark1,
+                            ordertime,
+                            strategyname,
+                            orderqty,
+                            orderdirection,
+                            orderoffset,
+                            entryprice,
+                            contract,
+                            exchangeid,
+                            orderdate
+                        )
+                        VALUES (
+                            '',
+                            '',
+                            '2019-10-23 21:18:18.000000',
+                            'DT_IntraDayCommonStrategy',
+                            1,
+                            '空',
+                            '平仓',
+                            622,
+                            'i2001',
+                            '',
+                            '2019-10-23'
+                        );
+
+
+INSERT INTO tradeorders (
+                            remark2,
+                            remark1,
+                            ordertime,
+                            strategyname,
+                            orderqty,
+                            orderdirection,
+                            orderoffset,
+                            entryprice,
+                            contract,
+                            exchangeid,
+                            orderdate
+                        )
+                        VALUES (
+                            '',
+                            '',
+                            '2019-10-24 10:18:18.000000',
+                            'DT_IntraDayCommonStrategy',
+                            1,
+                            '多',
+                            '平仓',
+                            8000,
+                            'AP001',
+                            '',
+                            '2019-10-24'
+                        );
+
+
+
+INSERT INTO tradeorders (
+                            remark2,
+                            remark1,
+                            ordertime,
+                            strategyname,
+                            orderqty,
+                            orderdirection,
+                            orderoffset,
+                            entryprice,
+                            contract,
+                            exchangeid,
+                            orderdate
+                        )
+                        VALUES (
+                            '',
+                            '',
+                            '2019-10-24 10:28:18.000000',
+                            'DT_IntraDayCommonStrategy',
+                            1,
+                            '空',
+                            '平仓',
+                            8000,
+                            'AP001',
+                            '',
+                            '2019-10-24'
+                        );
+
